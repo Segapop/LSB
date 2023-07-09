e464f8d58a428c561ef78e2a09a069fbb86a6962
Family <segapop4@gmail.com>
2023-07-08 23:35:12 -0500

    Workspace rename and backup

---
+++ src/main/java/com/lsb/client/ClientProxy.java
@@ -0,0 +1,51 @@
+package com.lsb.client;
+
+import com.lsb.client.entity.model.ModelMoissanite;
+import com.lsb.client.entity.render.RenderVaryTest;
+import com.lsb.lsb;
+import com.lsb.client.entity.model.ModelTest;
+import com.lsb.client.entity.model.ModelVaryTest;
+import com.lsb.client.entity.render.RenderTest;
+import com.lsb.init.AddonEntities;
+import com.lsb.init.AddonItemProperties;
+import com.lsb.init.AddonItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class ClientProxy {
+
+    @Mod.EventBusSubscriber(modid = lsb.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModBusEvents {
+
+
+        @SubscribeEvent
+        public static void onClientSetup(EntityRenderersEvent.RegisterRenderers event) {
+            event.registerEntityRenderer(AddonEntities.VARYTEST.get(), m -> new RenderVaryTest(m, new ModelVaryTest<>(m.bakeLayer(ModelVaryTest.LAYER_LOCATION))));
+            event.registerEntityRenderer(AddonEntities.TEST.get(), m -> new RenderTest(m, new ModelTest<>(m.bakeLayer(ModelMoissanite.LAYER_LOCATION))));
+        }
+
+        @SubscribeEvent
+        public static void doSetup(FMLClientSetupEvent event) {
+            AddonItemProperties.addCustomItemProperties();
+        }
+
+        @SubscribeEvent
+        public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+            //register layers here
+            event.registerLayerDefinition(ModelVaryTest.LAYER_LOCATION, ModelVaryTest::createBodyLayer);
+            event.registerLayerDefinition(ModelTest.LAYER_LOCATION, ModelTest::createBodyLayer);
+        }
+
+        @SubscribeEvent
+        public void buildContents(CreativeModeTabEvent.BuildContents event) {
+            if (event.getTab() == com.gempire.proxy.ClientProxy.GEMPIRE_GEMSTONES) {
+                event.accept(AddonItems.TEST_GEM.get());
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/lsb/lsb.java
@@ -0,0 +1,57 @@
+package com.lsb;
+
+import com.lsb.entity.entities.EntityTest;
+import com.lsb.init.AddonAbilities;
+import com.lsb.init.AddonEntities;
+import com.lsb.entity.entities.EntityVaryingTest;
+import com.lsb.init.RegistryHandler;
+import com.gempire.init.EventHandler;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import software.bernie.geckolib.GeckoLib;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(lsb.MODID)
+public class lsb
+{
+
+    public static final String MODID = "lsb";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public lsb()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+        // Register the commonSetup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::EntityAttributes);
+        modEventBus.addListener(this::commonSetup);
+
+        RegistryHandler.init();
+
+        GeckoLib.initialize();
+
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
+    }
+
+    public void EntityAttributes(final EntityAttributeCreationEvent event) {
+        //register entity attributes, these are set in the EntityTestGem class
+        event.put(AddonEntities.TEST.get(), EntityTest.registerAttributes().build());
+        event.put(AddonEntities.VARYTEST.get(), EntityVaryingTest.registerAttributes().build());
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        AddonEntities.setAddonGems();
+        AddonEntities.registerCruxes();
+        AddonAbilities.registerAbilities();
+    }
+}
