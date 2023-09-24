14bf64be00e4992d7779fa62ccf3dfdc9d7cd8f7
Family <segapop4@gmail.com>
2023-09-23 20:15:19 -0500

    Fix to LSB and restoring files, also removed Jade

--- src/main/java/com/lsb/init/AddonCruxes.java
+++ src/main/java/com/lsb/init/AddonCruxes.java
@@ -66,33 +66,6 @@ public class AddonCruxes {
         return new GemConditions(gemCruxes, essences, primer, gemTemperatureMin, gemTemperatureMax, rarity);
     }
 
-    public static GemConditions SBJADE_CONDITIONS(){
-        ArrayList<Crux> gemCruxes = new ArrayList<>();
-        //add temperatures, these are biome temperatures, reference other gems to set these
-        float gemTemperatureMin = -0.7f;
-        float gemTemperatureMax = 0.8f;
-        //add a primer
-        Item primer = Items.QUARTZ;
-        //add essences
-        String essences = "pink-blue";
-        //add crux blocks
-        gemCruxes.add(new Crux(Blocks.STONE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.TERRACOTTA.defaultBlockState(), 2, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.DEEPSLATE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.SANDSTONE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.RED_SANDSTONE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.GRANITE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.DIORITE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.MAGMA_BLOCK.defaultBlockState(), 3, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.IRON_ORE.defaultBlockState(), 3, CruxType.MINERAL, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.BASALT.defaultBlockState(), 9, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.SPONGE.defaultBlockState(), 9, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.NETHER_QUARTZ_ORE.defaultBlockState(), 16, CruxType.MINERAL, gemTemperatureMin, gemTemperatureMax));
-        //set a rarity to make the gem more/less rare
-        int rarity = 1;
-        return new GemConditions(gemCruxes, essences, primer, gemTemperatureMin, gemTemperatureMax, rarity);
-    }
-
     public static GemConditions SBMOONSTONE_CONDITIONS(){
         ArrayList<Crux> gemCruxes = new ArrayList<>();
         //add temperatures, these are biome temperatures, reference other gems to set these
@@ -407,33 +380,6 @@ public class AddonCruxes {
         int rarity = 4;
         return new GemConditions(gemCruxes, essences, primer, gemTemperatureMin, gemTemperatureMax, rarity);
     }
-
-    public static GemConditions SBPENTAGONITE_CONDITIONS(){
-        ArrayList<Crux> gemCruxes = new ArrayList<>();
-        //add temperatures, these are biome temperatures, reference other gems to set these
-        float gemTemperatureMin = 2.0f;
-        float gemTemperatureMax = 2.0f;
-        //add a primer
-        Item primer = Items.BONE;
-        //add essences
-        String essences = "pink-blue";
-        //add crux blocks
-        gemCruxes.add(new Crux(Blocks.STONE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.TERRACOTTA.defaultBlockState(), 2, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.DEEPSLATE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.SANDSTONE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.RED_SANDSTONE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.GRANITE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.DIORITE.defaultBlockState(), stoneCruxValue, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.MAGMA_BLOCK.defaultBlockState(), 3, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.IRON_ORE.defaultBlockState(), 3, CruxType.MINERAL, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.BASALT.defaultBlockState(), 9, CruxType.MINERAL, gemTemperatureMin, gemTemperatureMax));
-        gemCruxes.add(new Crux(Blocks.BONE_BLOCK.defaultBlockState(), 16, CruxType.INORGANIC, gemTemperatureMin, gemTemperatureMax));
-        //set a rarity to make the gem more/less rare
-        int rarity = 4;
-        return new GemConditions(gemCruxes, essences, primer, gemTemperatureMin, gemTemperatureMax, rarity);
-    }
-
 
     public static GemConditions SBCUPRITE_CONDITIONS(){
         ArrayList<Crux> gemCruxes = new ArrayList<>();
--- src/main/java/com/lsb/init/AddonItems.java
+++ src/main/java/com/lsb/init/AddonItems.java
@@ -11,41 +11,165 @@ import net.minecraftforge.registries.RegistryObject;
 public class AddonItems {
      public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, lsb.MODID);
 
-     //Jade
-     public static final RegistryObject<Item> WHITE_SBJADE_GEM = ITEMS.register("sbjade_gem_0", () ->
+//Moissanite
+     public static final RegistryObject<Item> WHITE_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_0", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> ORANGE_SBJADE_GEM = ITEMS.register("sbjade_gem_1", () ->
+     public static final RegistryObject<Item> ORANGE_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_1", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> MAGENTA_SBJADE_GEM = ITEMS.register("sbjade_gem_2", () ->
+     public static final RegistryObject<Item> MAGENTA_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_2", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> LIGHT_BLUE_SBJADE_GEM = ITEMS.register("sbjade_gem_3", () ->
+     public static final RegistryObject<Item> LIGHT_BLUE_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_3", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> YELLOW_SBJADE_GEM = ITEMS.register("sbjade_gem_4", () ->
+     public static final RegistryObject<Item> YELLOW_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_4", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> LIME_SBJADE_GEM = ITEMS.register("sbjade_gem_5", () ->
+     public static final RegistryObject<Item> LIME_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_5", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> PINK_SBJADE_GEM = ITEMS.register("sbjade_gem_6", () ->
+     public static final RegistryObject<Item> PINK_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_6", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> GRAY_SBJADE_GEM = ITEMS.register("sbjade_gem_7", () ->
+     public static final RegistryObject<Item> GRAY_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_7", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> LIGHT_GRAY_SBJADE_GEM = ITEMS.register("sbjade_gem_8", () ->
+     public static final RegistryObject<Item> LIGHT_GRAY_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_8", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> CYAN_SBJADE_GEM = ITEMS.register("sbjade_gem_9", () ->
+     public static final RegistryObject<Item> CYAN_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_9", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> PURPLE_SBJADE_GEM = ITEMS.register("sbjade_gem_10", () ->
+     public static final RegistryObject<Item> PURPLE_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_10", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> BLUE_SBJADE_GEM = ITEMS.register("sbjade_gem_11", () ->
+     public static final RegistryObject<Item> BLUE_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_11", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> BROWN_SBJADE_GEM = ITEMS.register("sbjade_gem_12", () ->
+     public static final RegistryObject<Item> BROWN_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_12", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> GREEN_SBJADE_GEM = ITEMS.register("sbjade_gem_13", () ->
+     public static final RegistryObject<Item> GREEN_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_13", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> RED_SBJADE_GEM = ITEMS.register("sbjade_gem_14", () ->
+     public static final RegistryObject<Item> RED_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_14", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     public static final RegistryObject<Item> BLACK_SBJADE_GEM = ITEMS.register("sbjade_gem_15", () ->
+     public static final RegistryObject<Item> BLACK_SBMOISSANITE_GEM = ITEMS.register("sbmoissanite_gem_15", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
-     //Pentagonite
-     public static final RegistryObject<Item> SBPENTAGONITE_GEM = ITEMS.register("sbpentagonite_gem", () ->
+     //Opal
+     public static final RegistryObject<Item> COOBER_PEDY_SBOPAL_GEM = ITEMS.register("sbopal_gem_0", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> SLOVAKIAN_SBOPAL_GEM = ITEMS.register("sbopal_gem_1", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> PERUVIAN_SBOPAL_GEM = ITEMS.register("sbopal_gem_2", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> ETHIOPIAN_SBOPAL_GEM = ITEMS.register("sbopal_gem_3", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> FIRE_SBOPAL_GEM = ITEMS.register("sbopal_gem_4", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> TANZANIAN_SBOPAL_GEM = ITEMS.register("sbopal_gem_5", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> ROSE_WATER_SBOPAL_GEM = ITEMS.register("sbopal_gem_6", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> MINTABIE_SBOPAL_GEM = ITEMS.register("sbopal_gem_7", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> CRYSTAL_SBOPAL_GEM = ITEMS.register("sbopal_gem_8", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> CONTRALUZ_SBOPAL_GEM = ITEMS.register("sbopal_gem_9", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> MORADO_SBOPAL_GEM = ITEMS.register("sbopal_gem_10", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> HARLEQUIN_SBOPAL_GEM = ITEMS.register("sbopal_gem_11", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BOULDER_SBOPAL_GEM = ITEMS.register("sbopal_gem_12", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> ANDAMOOKA_SBOPAL_GEM = ITEMS.register("sbopal_gem_13", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> CHERRY_SBOPAL_GEM = ITEMS.register("sbopal_gem_14", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> HONDURAN_SBOPAL_GEM = ITEMS.register("sbopal_gem_15", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Moonstone
+     public static final RegistryObject<Item> WHITE_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_0", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> ORANGE_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_1", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> MAGENTA_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_2", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> LIGHT_BLUE_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_3", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> YELLOW_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_4", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> LIME_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_5", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> PINK_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_6", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> GRAY_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_7", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> LIGHT_GRAY_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_8", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> CYAN_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_9", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> PURPLE_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_10", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BLUE_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_11", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BROWN_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_12", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> GREEN_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_13", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> RED_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_14", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BLACK_SBMOONSTONE_GEM = ITEMS.register("sbmoonstone_gem_15", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Gold
+     public static final RegistryObject<Item> WHITE_SBGOLD_GEM = ITEMS.register("sbgold_gem_0", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> YELLOW_SBGOLD_GEM = ITEMS.register("sbgold_gem_4", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> PINK_SBGOLD_GEM = ITEMS.register("sbgold_gem_6", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Pyrite
+     public static final RegistryObject<Item> SBPYRITE_GEM = ITEMS.register("sbpyrite_gem", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Platinum
+     public static final RegistryObject<Item> SBPLATINUM_GEM = ITEMS.register("sbplatinum_gem", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Copper
+     public static final RegistryObject<Item> COPPER_SBCOPPER_GEM = ITEMS.register("sbcopper_gem_1", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> OXIDIZED_SBCOPPER_GEM = ITEMS.register("sbcopper_gem_9", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> EXPOSED_SBCOPPER_GEM = ITEMS.register("sbcopper_gem_12", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> WEATHERED_SBCOPPER_GEM = ITEMS.register("sbcopper_gem_13", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Silver
+     public static final RegistryObject<Item> SBSILVER_GEM = ITEMS.register("sbsilver_gem", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Jeremejevite
+     public static final RegistryObject<Item> SBJEREMEJEVITE_GEM = ITEMS.register("sbjeremejevite_gem", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     //Zirconia
+     public static final RegistryObject<Item> WHITE_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_0", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> ORANGE_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_1", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> MAGENTA_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_2", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> LIGHT_BLUE_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_3", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> YELLOW_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_4", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> LIME_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_5", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> PINK_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_6", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> GRAY_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_7", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> LIGHT_GRAY_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_8", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> CYAN_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_9", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> PURPLE_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_10", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BLUE_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_11", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BROWN_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_12", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> GREEN_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_13", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> RED_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_14", () ->
+             new ItemGem(new Item.Properties(), lsb.MODID));
+     public static final RegistryObject<Item> BLACK_SBZIRCONIA_GEM = ITEMS.register("sbzirconia_gem_15", () ->
              new ItemGem(new Item.Properties(), lsb.MODID));
      //Ammolite
      public static final RegistryObject<Item> SBAMMOLITE_GEM = ITEMS.register("sbammolite_gem", () ->
