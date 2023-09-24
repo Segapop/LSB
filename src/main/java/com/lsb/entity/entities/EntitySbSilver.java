6c7a8cef3bd2d59553a18969373a88e59e81fb9c
Family <segapop4@gmail.com>
2023-09-23 20:24:45 -0500

    Fix to LSB and restoring files, also removed Jade

---
+++ src/main/java/com/lsb/entity/entities/EntitySbSilver.java
@@ -0,0 +1,237 @@
+package com.lsb.entity.entities;
+
+import com.gempire.entities.abilities.*;
+import com.gempire.entities.abilities.base.Ability;
+import com.gempire.entities.ai.*;
+import com.gempire.entities.bases.EntityGem;
+import com.gempire.entities.other.EntityAbomination;
+import com.gempire.entities.other.EntityCrawler;
+import com.gempire.entities.other.EntityShambler;
+import com.gempire.util.GemPlacements;
+import com.lsb.entity.abilities.ConductorAbility;
+import com.lsb.init.AddonItems;
+import com.lsb.lsb;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+import java.util.ArrayList;
+
+public class EntitySbSilver extends EntityGem {
+
+    //this gem was copied from ruby
+
+    public EntitySbSilver(EntityType<? extends PathfinderMob> type, Level worldIn) {
+        super(type, worldIn);
+    }
+
+    public static AttributeSupplier.Builder registerAttributes() {
+        return Mob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 10.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.4D)
+                .add(Attributes.ATTACK_DAMAGE, 1.0D)
+                .add(Attributes.ATTACK_SPEED, 1.0D);
+    }
+
+    @Override
+    public String getModID() {
+        //THIS DEFINES THE GEM AS BEING AN ADDON GEM
+        return lsb.MODID;
+    }
+
+    @Override
+    public Class getItemRegister() {
+        //THIS TOO
+        return AddonItems.class;
+    }
+
+    @Override
+    public boolean isPopular() {
+        return false;
+    }
+
+    public boolean flocksTo(EntityGem gem) {
+        return gem.isPopular() || gem instanceof EntitySbGold;
+    }
+
+    @Override
+    public Float baseXScale() {
+        return .8F;
+    }
+
+    @Override
+    public Float baseYScale() {
+        return .8F;
+    }
+
+    @Override
+    public Float baseZScale() {
+        return .8F;
+    }
+
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(7, new FloatGoal(this));
+        this.goalSelector.addGoal(6, new PanicGoal(this, 1.1D));
+        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 4.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(8, new EntityAIWander(this, 1.0D));
+        this.goalSelector.addGoal(7, new EntityAIFollowGarnet(this, 0.7D));
+        this.goalSelector.addGoal(7, new EntityAIFollowAssigned(this, 1.0D));
+        this.goalSelector.addGoal(7, new EntityAIFollowOwner(this, 1.0D));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityGem.class, 1, false, false, this::checkRebel));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityAbomination.class, 1, false, false, this::checkNotSludged));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityShambler.class, 1, false, false, this::checkNotSludged));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityCrawler.class, 1, false, false, this::checkNotSludged));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Mob.class, 1, false, false, (p_234199_0_) -> p_234199_0_.getClassification(true) == MobCategory.MONSTER));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.1D, false));
+        this.targetSelector.addGoal(1, new OwnerHurtByTargetGemGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGemGoal(this));
+        this.goalSelector.addGoal(1, new EntityAISludged(this, 0.6));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, EntityGem.class, 1, false, false, this::checkBothSludged));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 1, false, false, this::checkSludged));
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, EntityGem.class, 6.0F, 1.0D, 1.2D, this::checkElseSludged));
+    }
+
+    @Override
+    public int generateSkinVariant() {
+        return 0;
+    }
+
+    @Override
+    public GemPlacements[] getPlacements() {
+        return new GemPlacements[]{
+                GemPlacements.TOP_OF_HEAD, GemPlacements.FOREHEAD, GemPlacements.BACK_OF_HEAD, GemPlacements.LEFT_EYE, GemPlacements.RIGHT_EYE, GemPlacements.NOSE,
+                GemPlacements.LEFT_CHEEK, GemPlacements.RIGHT_CHEEK, GemPlacements.LEFT_EAR, GemPlacements.RIGHT_EAR, GemPlacements.CHEST, GemPlacements.BACK, GemPlacements.BELLY,
+                GemPlacements.LEFT_SHOULDER, GemPlacements.RIGHT_SHOULDER, GemPlacements.LEFT_HAND, GemPlacements.RIGHT_HAND, GemPlacements.LEFT_PALM, GemPlacements.RIGHT_PALM,
+                GemPlacements.LEFT_THIGH, GemPlacements.RIGHT_THIGH, GemPlacements.LEFT_ANKLE, GemPlacements.RIGHT_ANKLE
+        };
+    }
+
+    @Override
+    public SoundEvent getInstrument()
+    {
+        return SoundEvents.NOTE_BLOCK_BASEDRUM.get();
+    }
+    @Override
+    public int generateHairVariant() {
+        return this.random.nextInt(4);
+    }
+
+    @Override
+    public int generateInsigniaColor() {
+        return 7;
+    }
+
+    @Override
+    public int generateOutfitColor() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasOutfitPlacementVariant() {
+        return false;
+    }
+
+    @Override
+    public int[] outfitPlacementVariants() {
+        return new int[]{
+        };
+    }
+
+    public ArrayList<Ability> possibleAbilities(){
+        ArrayList<Ability> arrayList = new ArrayList<>();
+        arrayList.add(new AbilityZilch());
+        arrayList.add(new AbilityUnhinged());
+        arrayList.add(new AbilityKnockback());
+
+        return arrayList;
+    }
+    public ArrayList<Ability> definiteAbilities(){
+        ArrayList<Ability> arrayList = new ArrayList<>();
+        arrayList.add(new ConductorAbility());
+        return arrayList;
+    }
+
+    public int generateSkinColorVariant() {
+        return 0;
+    }
+
+    @Override
+    public boolean generateIsEmotional() {
+        return true;
+    }
+
+    @Override
+    public byte EmotionThreshold() {
+        return 10;
+    }
+
+    public boolean canChangeUniformColorByDefault() {
+        return true;
+    }
+
+    public boolean canChangeInsigniaColorByDefault(){
+        return true;
+    }
+
+    @Override
+    public boolean fireImmune(){
+        return false;
+    }
+
+    public boolean hasSkinColorVariant(){
+        return false;
+    }
+
+    @Override
+    public int generateOutfitVariant() {
+        return this.random.nextInt(1);
+    }
+
+    @Override
+    public int generateInsigniaVariant() {
+        return this.getOutfitVariant();
+    }
+
+    public int generateRebelInsigniaVariant() {
+        return this.getRebelOutfitVariant();
+    }
+
+    @Override
+    public int generateVisorVariant() {
+        if (getHairVariant() == 2)
+            return 0;
+        return this.random.nextInt(2);
+    }
+
+    @Override
+    public int baseFocus() {
+        return 6;
+    }
+
+    public int generateHardness() {
+        return 2;
+    }
+
+
+
+    @Override
+    public int getColor() {
+        return 14;
+    }
+
+    @Override
+    public int exitHoleSize() {
+        return 2;
+    }
+}
